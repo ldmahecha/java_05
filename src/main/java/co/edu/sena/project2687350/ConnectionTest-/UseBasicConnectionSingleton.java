88b03affd615eba82dad45c8e38b9a87185085e7
dani <dani@siy.sena.edu.co>
2023-05-05 17:23:58 -0500

    trabajo hoy

---
+++ src/main/java/co/edu/sena/project2687350/ConnectionTest-/UseBasicConnectionSingleton.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class UseBasicConnectionSingleton {
+    public static void main(String[] args) throws
+            SQLException {
+        try (Connection conn = BasicConnectionSingleton.getInstance();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM myapp.users_tbl")) {
+ while (rs.next()) {
+
+            System.out.print(rs.getString("user_firstname")
+            );
+            System.out.print(" | ");
+
+            System.out.println(rs.getString("user_lastname"
+            ));
+        }
+    } // end try
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+} // UseBasicConnectionSingleton
